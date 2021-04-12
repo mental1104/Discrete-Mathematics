9c26ee4ae51e04d26464564978a2b8c712449994
mental1104 <mental1104@gmail.com>
2021-04-12 19:18:42 +0800

    Computer Project 1.3

---
+++ chapter1/readme.md
@@ -0,0 +1,50 @@
+# Chapter 1 The Foundations: Logic and Proofs  
+
+This is the solutions of **computer project and computer exploration** of Chapter 1.  
+
+---
+
+### 1.Given the truth values of the propositions p and q, find the truth values of the conjunction, disjunction, exclusive or,conditional statement, and biconditional of these propositions.  
+
+[code](./project_1.cpp)  
+
+Usage && Example:  
+![](./pic/1.png)  
+
+
+
+### 2.Given two bit strings of length n, find the bitwise AND, bitwise OR, and bitwise XOR of these strings.  
+[code](./project_2.cpp)  
+Usage:   
+
+Enter the n(length of string) behind the executable file.  
+
+Example:  
+
+![](./pic/2.png)
+
+
+### 3.Give a compound proposition, determine whether it is satisfiable by checking its truth value for all positive assignments of truth values to its propositional variables
+
+[code](./project_3.cpp)  
+
+Usage:  
+> negation '!'  
+> disjunction: '|'  
+> Conjunction: '&'  
+> implication: '->'  
+> equivalence: '<->'  
+
+Examples(Exercise 1.3.65):  
+![](./pic/ex65.jpg)
+a:  
+![](./pic/3a.png)  
+b:  
+![](./pic/3b.png)  
+c:  
+![](./pic/3c.png)  
+
+In addition, you can compile the [truth.cpp](./truth.cpp) to print out the truth table of a proposition.  
+To see what final results would every combination of truth value in Exercise 1.3.65 yield:  
+![](./pic/truth.png)  
+
---
+++ chapter1/truth.cpp
@@ -0,0 +1,173 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<unordered_map>
+
+using namespace::std;
+stack<char> Stack;
+stack<bool> P;
+vector<char> RPN;
+unordered_map<char, bool> value;
+int cnt;
+
+bool p[26];//To store the value of preposition.
+vector<char> n;//name of prepostion
+
+
+bool deny(bool p) { return !p; }
+bool And(bool p, bool q) { return p&&q; }
+bool Or(bool p, bool q) {return p||q; }
+bool contain(bool p, bool q){return !(p == true && q == false); }
+bool equal(bool p, bool q) { return p == q;}
+
+
+bool judge(char p, char q){
+    int x = 0;
+    int y = 0;
+    if(p == '!')
+        x = 5;
+    else if(p == '&')
+        x = 4;
+    else if(p == '|')
+        x = 3;
+    else if(p == '-')
+        x = 2;
+    else if(p == '<')
+        x = 1;
+    if(q == '!')
+        y = 5;
+    else if(q == '&')
+        y = 4;
+    else if(q == '|')
+        y = 3;
+    else if(q == '-')
+        y = 2;
+    else if(q == '<')
+        y = 1;
+    
+    return x > y;
+}
+
+
+
+void evaluate(string s){
+    int size = s.size();
+    int i = 0;
+    while(i<size){
+        if(s[i] >='a' && s[i] <='z'){
+            RPN.push_back(s[i]);
+            vector<char>::iterator iter = find(n.begin(), n.end(), s[i]);
+            if(iter == n.end())
+                n.push_back(s[i]);
+            i++;
+        }
+        else{
+            if(s[i] == '('){
+                Stack.push(s[i]);
+                i++;
+            }
+            else if(s[i] == ')'){
+                while(Stack.top()!='('){
+                    RPN.push_back(Stack.top());
+                    Stack.pop();
+                }
+                Stack.pop();
+                i++;
+            }
+            else{
+                if(Stack.empty()){
+                    Stack.push(s[i]);
+                    if(s[i] == '-') i++;
+                    if(s[i] == '<') i=i+2;
+                    i++;
+                }else{
+                    while(!judge(s[i],Stack.top())){
+                        RPN.push_back(Stack.top());
+                        Stack.pop();
+                        if(Stack.empty())
+                            break;
+                    }
+                    Stack.push(s[i]);
+                    if(s[i] == '-') i++;
+                    if(s[i] == '<') i=i+2;
+                    i++;
+                }
+            }
+        }
+    }
+    while(!Stack.empty()){
+        if(Stack.top()!='(' && Stack.top()!=')')
+            RPN.push_back(Stack.top());
+        Stack.pop();
+    }
+}
+
+bool yield(){
+    while(!P.empty())
+        P.pop();
+    
+    int size = RPN.size();
+    for(int i = 0; i<size; i++){
+        bool x,y;
+        if(RPN[i] >='a' && RPN[i] <='z')
+            P.push(value[RPN[i]]);
+            
+        else if(RPN[i] == '!'){
+            x = P.top();
+            P.pop();
+            P.push(deny(x));
+        }else{
+            x = P.top();
+            P.pop();
+            y = P.top();
+            P.pop();
+            if(RPN[i] == '&')
+                P.push(And(x,y));
+            else if(RPN[i] == '|')
+                P.push(Or(x,y));
+            else if(RPN[i] == '-')
+                P.push(contain(y,x));
+            else if(RPN[i] == '<')
+                P.push(equal(x,y));
+        }
+    }
+    return P.top();
+}
+
+void dfs(int seed){
+    if(seed == cnt){
+        for(int i = 0; i<cnt; i++)
+            cout << value[n[i]] << " ";
+        cout << "       " << yield() << endl;
+        return;
+    }
+    value[n[seed]] = true;
+    dfs(seed+1);
+    value[n[seed]] = false;
+    dfs(seed+1);
+
+    /*p[seed] = 1;
+    dfs(seed+1);
+    p[seed] = 0;
+    dfs(seed+1);*/
+}
+
+int main(void){
+    string sourceString;
+    cout << "Please enter a compound proposition." << endl << "> ";
+    cin >> sourceString;
+    evaluate(sourceString);
+    cout << "RPN: ";
+    for(auto i: RPN){
+        cout << i;
+    }
+    cout << endl;
+    cnt = n.size();
+    for(auto i: n)
+        cout << i << " ";
+    cout << "       " << sourceString << endl;
+    dfs(0);
+    return 0;
+}
